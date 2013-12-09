6725857649fee7e906e30e74eb6589d74be29007
wheelere <earnestw@Earnests-MacBook-Air.local>
2013-12-08 20:16:43 -0600

    ima commit inBrace
    i wrote inBrace it took me way too long

--- src/edu/grinnell/csc207/lewismar/json/JSONObject.java
+++ src/edu/grinnell/csc207/lewismar/json/JSONObject.java
@@ -4,16 +4,36 @@ public class JSONObject {
 
     String key;
     Object value;
-    
+
     public JSONObject(String str) {
 	String[] strs = str.split("\"");
 	this.key = strs[1];
-	
+
 	if (strs[2].contains("[")) {
-	    
+
 	}
 	if (strs[2].contains("{")) {
-	    value = new JSONObject(str.inBrace);
+	    value = new JSONObject(inBrace(str));
 	}
     }
+
+    /**
+     * takes a string that begins with { and returns the substring between that
+     * brace and its counterpart
+     */
+    private static String inBrace(String str) {
+	int open = 1;
+	char c;
+	int i = 0;
+	while (open > 0) {
+	    c = str.charAt(++i);
+	    if (c == '{') {
+		open++;
+	    } else if (c == '}') {
+		open--;
+	    } // if/else if
+	} // while
+	return str.substring(1, i);
+    } // inBrace(String)
+
 }
