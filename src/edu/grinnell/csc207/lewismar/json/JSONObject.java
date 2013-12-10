57171ddb4a9e46dc92646b6e0e81ca05a0f4c9ba
Mark Lewis <lewismar@grinnell.edu>
2013-12-09 20:37:56 -0600

    Changed approach, lots of work still needed.

--- src/edu/grinnell/csc207/lewismar/json/JSONObject.java
+++ src/edu/grinnell/csc207/lewismar/json/JSONObject.java
@@ -4,27 +4,69 @@ public class JSONObject {
 
     String key;
     Object value;
+    static int i = 0; // global variable to traverse the string
 
-    public JSONObject(String str) {
-	String[] strs = str.split("\"");
-	this.key = strs[1];
+    public JSONObject(String str) throws Exception {
 
-	if (strs[2].contains("[")) {
-
+	// Find the key
+	i = 1; // to traverse str, start at 1 because 0 is a quote
+	while (str.charAt(i) != '"' && str.charAt(i-1) != '\\') {
+	    key += str.charAt(i);
+	    i++;
+	} // while
+	  // were at a colon
+	while (str.charAt(i) == ' ') {
+	    i++;
+	} // while
+	  
+	// Cases for value:
+	  // We have a string
+	if (str.charAt(i) == '\"') {
+	    this.value = onString(str);
 	}
-	if (strs[2].contains("{")) {
+	// We have digits
+	else if (Character.isDigit(str.charAt(i))) {
+	    String toValue = "";
+	    while (i < str.length()) {
+		toValue += str.charAt(i);
+		i++;
+	    } // while
+	    this.value = new Double(toValue);
+	}
+	// We have another object
+	else if (str.charAt(i) == '{')
 	    value = new JSONObject(inBrace(str));
+	// we have an array
+	
+	// otherwise fail
+	else {
+	    throw new Exception("invalid input");
 	}
-    }
 
+    }
+    private String onString(String str) {
+	// String to return
+	String tmp = "";
+	// move past quote
+	i++; 
+	// i is currently at the quote
+	while (str.charAt(i) != '\"' && str.charAt(i - 1) != '\\') {
+	    tmp += str.charAt(i);
+	    i++;
+	}
+	return tmp;
+
+    }
     /**
      * takes a string that begins with { and returns the substring between that
      * brace and its counterpart
      */
     private static String inBrace(String str) {
+	int initial = i + 1;
 	int open = 1;
 	char c;
-	int i = 0;
+	// currently at a brace
+	i++;
 	while (open > 0) {
 	    c = str.charAt(++i);
 	    if (c == '{') {
@@ -33,7 +75,7 @@ public class JSONObject {
 		open--;
 	    } // if/else if
 	} // while
-	return str.substring(1, i);
+	return str.substring(initial, i - 1);
     } // inBrace(String)
 
 }
